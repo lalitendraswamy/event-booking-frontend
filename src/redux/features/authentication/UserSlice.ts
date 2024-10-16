f9fc73a8660f5e5dd7b1d254425149c5bf4d3304
John Benny Bob Kancharla <bennykancharla444@gmail.com>
2024-10-16 15:39:37 +0530

    Added User slices and thunk in redux

--- src/redux/features/authentication/UserSlice.ts
+++ src/redux/features/authentication/UserSlice.ts
@@ -1,42 +1,71 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
-interface User {
-    id: number;
-    name: string;
-    email: string;
+export interface User {
+    userId:string,
+    username:string,
+    email:string,
+    role:string
 }
 
-interface UserState {
+export interface UserState {
     users: User[];
+    user: User | null;
     loading: boolean;
     error: string | null;
 }
 
 const initialState: UserState = {
     users: [],
+    user:null,
     loading: false,
     error: null,
 };
 
-// export const fetchUsers = createAsyncThunk<User[], void>(
-//     'users/fetchUsers',
-//     async () => {
-//         const response = await fetch('https://jsonplaceholder.typicode.com/users');
-//         return response.json();
-//     }
-// );
+export const getUsers = createAsyncThunk<User[], void>(
+    'users/getUsers',
+    async () => {
+        try{
+            const response = await axios.get('http://localhost:5000/users');
+            return response.data;
+        }catch(e){
+            console.log(e);
+        }
+    }
+);
+
+export const postUser = createAsyncThunk<User,User>(
+    'users/postUser',
+    async (user,{rejectWithValue}) => {
+        try{
+            const response = await axios.post("http://localhost:5000/users",user);
+            return response.data
+        }catch(error:any){
+            console.log(error);
+            // return e 
+            return rejectWithValue(
+                error.response ? error.response.data : "Something went wrong"
+            );
+        }
+    }
+)
 
 const userSlice = createSlice({
     name: 'users',
     initialState,
     reducers: {},
-    // extraReducers: (builder) => {
-    //     builder
-    //         .addCase(fetchUsers.fulfilled, (state, action:any) => {
-    //             state.loading = false;
-    //             state.users = action.payload;
-    //         })
-    // },
+    extraReducers: (builder) => {
+        builder
+            .addCase(getUsers.fulfilled, (state, action:any) => {
+                state.loading = false;
+                state.users = action.payload;
+            })
+            .addCase(postUser.fulfilled,(state,action) => {
+                console.log("added User")
+                state.users.push(action.payload)
+
+            })
+    },
 });
 
 export default userSlice.reducer;
