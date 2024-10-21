ec1a52383f0cd38c8de75c76c1fe6a7808bda010
John Benny Bob Kancharla <bennykancharla444@gmail.com>
2024-10-21 11:18:17 +0530

    orders retrivel by each user

--- src/components/MyOrdersPage/MyOrdersPage.tsx
+++ src/components/MyOrdersPage/MyOrdersPage.tsx
@@ -5,13 +5,14 @@ import OrderItem from '../shared/orderItem/orderItem'
 import { getAllOrders, getOrders } from '../../redux/features/authentication/OrderSlice'
 import './my-orders-page.css'
 import { useDispatch, useSelector } from 'react-redux'
+import { getCookie } from '../../utils/cookieUtils'
 
 export default function MyOrdersPage() {
   const {orders} = useSelector((state: any) => state.orders);
   const dispatch = useDispatch()
-
+  const userId = getCookie('userId');
   useEffect(() => {
-    dispatch<any>(getOrders())
+    dispatch<any>(getOrders(userId))
   },[])
 
   
